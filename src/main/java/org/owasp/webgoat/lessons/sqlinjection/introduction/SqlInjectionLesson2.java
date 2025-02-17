24ebf51471a6b2f52394cb5bb21f62573e8f8355
franktomasello <franktomasello@outlook.com>
2025-02-17 15:55:06 -0800

    created test branch

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//test 
+
 @RestController
 @AssignmentHints(
     value = {
